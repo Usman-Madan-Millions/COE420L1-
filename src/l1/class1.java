2f095736609a6181a7ce59e0716c980adce77b22
Abdullah <b00090126@aus.edu>
2023-02-06 17:47:13 +0400

    Feature B added

--- src/l1/class1.java
+++ src/l1/class1.java
@@ -3,7 +3,12 @@ package l1;
 public class class1 {
 
 	public static void main(String[] args) {
-System.out.print("Hello World");
+    System.out.println("Hello World");
+	FeatureB();
 	}
 
+	public static void FeatureB() {
+		System.out.println("HEY! Feature B added");
+	}
+	
 }
